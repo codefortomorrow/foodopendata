fdc9c8c22a1783f255554bd4ecac64a286833e08
Mike Cheng - nsmalab <xuehung@gmail.com>
2013-09-27 09:33:48 +0800

    updated domain

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/foodopendata/"
-http_images_path = "/foodopendata/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/foodopendata/fonts"
-css_dir = "public/foodopendata/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
